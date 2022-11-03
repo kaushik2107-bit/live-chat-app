516a06ceb41201281c09c9bf6e3aa3351b4008d0
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-11-03 17:03:07 +0530

    third commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,17 +24,19 @@ export default function Login() {
 		)
 	}, [])
 
-
-
 	return (
-		<div className="flex w-[252px] h-[58px] bg-blue-600 items-center rounded-lg m-auto mt-[45vh] cursor-pointer" onClick={googleAuth}>
-			<div 
-				style={{background: "url(https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1)", backgroundSize: "cover", backgroundPosition: "center"}} 
-				className="w-[50px] h-[50px] m-1 rounded-lg" 
-			/>
-			<div className="w-[200px] h-[50px] bg-blue-600 m-1 flex items-center justify-center text-white font-semibold rounded-lg">
-				Sign Up With Google
-			</div>
-		</div>
+		<div id="signInDiv"></div>
 	)
+
+	// return (
+	// 	<div className="flex w-[252px] h-[58px] bg-blue-600 items-center rounded-lg m-auto mt-[45vh] cursor-pointer" onClick={googleAuth}>
+	// 		<div 
+	// 			style={{background: "url(https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1)", backgroundSize: "cover", backgroundPosition: "center"}} 
+	// 			className="w-[50px] h-[50px] m-1 rounded-lg" 
+	// 		/>
+	// 		<div className="w-[200px] h-[50px] bg-blue-600 m-1 flex items-center justify-center text-white font-semibold rounded-lg">
+	// 			Sign Up With Google
+	// 		</div>
+	// 	</div>
+	// )
 }
