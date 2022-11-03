3dbedd47bdbd1b86a3d63d1d0e6da678dd1510f0
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-11-03 17:00:13 +0530

    third commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import jwt_decode from "jwt-decode"
 
 export default function Login() {
 	const REACT_APP_API_URL = "https://live-socket-chat-app.herokuapp.com"
 	const googleAuth = () => {
 		window.open(`${REACT_APP_API_URL}/auth/google/callback`, "_self")
 	}
+
+	function handleCallbackResponse(res) {
+		console.log(res.credential)
+		var userObj = jwt_decode(res.credential)
+		console.log(userObj)
+	}
+
+	useEffect(() => {
+		google.accounts.id.initialize({
+			client_id: "965373897090-ddtpj3n4ra8ptdtso20953hit3fhnal4.apps.googleusercontent.com",
+			callback: handleCallbackResponse
+		})
+		google.accounts.id.renderButton(
+			document.getElementById("signInDiv"),
+			{ theme: "outline", size: "large" }
+		)
+	}, [])
+
+
+
 	return (
 		<div className="flex w-[252px] h-[58px] bg-blue-600 items-center rounded-lg m-auto mt-[45vh] cursor-pointer" onClick={googleAuth}>
 			<div 
