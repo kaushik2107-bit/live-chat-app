f377e4486e9014c13dbbd433bccb10e15b6032c2
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-11-03 14:36:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import MainPage from "./components/MainPage"
+import Login from "./components/Login"
+import { useEffect, useState } from "react"
+import { Routes, Route, Navigate } from "react-router-dom"
+import axios from "axios"
 
 function App() {
+  const [user, setUser] = useState(null);
+  const REACT_APP_API_URL = "https://live-socket-chat-app.herokuapp.com/"
+
+  const getUser = async () => {
+    try {
+      const url = `${REACT_APP_API_URL}/auth/login/success`
+      const { data } = await axios.get(url, { crossDomain: true, withCredentials: true })
+      setUser(data.user._json)
+      sessionStorage.setItem("name", data.user._json.name)
+      sessionStorage.setItem("email", data.user._json.email)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[100vw] h-[100vh] bg-[#202029] overflow-hidden">
+      <Routes>
+        <Route exact path="/" element={user ? <Navigate to="/chat" /> : <Navigate to="/login" /> } />
+        <Route exact path="/login" element={user ? <Navigate to="/chat" /> : <Login /> } />
+        <Route path="/chat" element={user ? <MainPage userInfo={user}/> : <Navigate to="/" /> } />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/useInfiniteScrolling.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+export default function useInfiniteScrolling(pageNumber) {
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(false)
+	const [msg, setMsg] = useState([])
+	const [hasMore, setHasMore] = useState(false)
+
+	// useEffect(() => {
+	// 	setMsg([])
+	// }, [pageNumber])
+	
+	useEffect(() => {
+		setLoading(true)
+		setError(false)
+		axios.get(`http://localhost:8000/api/fetch/${pageNumber}`)
+		.then(res => {
+			setMsg(prev => {
+				return [...prev, ...res.data.data]
+			})
+			setHasMore(res.data.data.length > 0)
+			setLoading(false)
+		})
+		.catch(e => {
+			setError(true)
+			console.log(e)
+		})
+	}, [pageNumber])
+
+	return { loading, error, msg, hasMore }
+}
